57f52cd73fa33cdd78968599847f80ef238df42d
Juan Carlos Faneite <juan.faneite@globant.com>
2025-05-19 16:15:35 -0400

    enhanced prompt

--- src/services/chatgpt.service.js
+++ src/services/chatgpt.service.js
@@ -4,9 +4,21 @@ const { OpenAI } = require("openai");
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 async function sendToChatGPT(document) {
-  const prompt = `Summarize this content by the doc_id which represent a document with its respectives pages represented by the page_number, build a json object with: { "title":"where you Give me in 10 words what is the general topic written in all the pages", then an array of highlights, relevant information that you consider important written in all the pages like: [{ "date": "Give me the date of this highlight", "description": "give me a summarize explanation in no more than 30 words of this highlight", "source": "give me the document source", "relevance": "give me the relevance of the event" }], documentSources: [array of string with the names of all the previous added in the source attribute do not repeat the source]} 
+  const prompt = `
+    understand this content by the doc_id which represent a document with its respectives pages represented by the page_number, 
+    Summarize the document in useful metadata as highlights, 
+    build a stringify json object with this format: 
+    array of hightlights 
+    [{ 
+      "title":"Give me in 10 words the title of the hightlight",
+      "date": "Give me the date of this highlight using the format YYYY-MM-DD",
+      "description": "give me a summarize explanation in no more than 50 words of this highlight", 
+      "source": "give me the title of the document source if the document does not have a title give it one related of all pages", 
+      "relevance": "give me the relevance of this hightlight" 
+    }]
 
-important to follow The "relevance" attribute could have this possible values array of numbers [number] the meaning of those numbers are [1: "very low", 2: "low", 3: "medium", 4: "high", 5: "very high"]`;
+    important to follow The "relevance" attribute could have this possible value: number and here is the meaning [1: "very low", 2: "low", 3: "medium", 4: "high", 5: "very high"]`;
+
   const content = `${prompt}\n\n${JSON.stringify(document)}`;
 
   const chatResponse = await openai.chat.completions.create({
